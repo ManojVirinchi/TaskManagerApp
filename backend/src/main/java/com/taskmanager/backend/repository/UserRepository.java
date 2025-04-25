75d32c90590616fd6bd97da8bf2f4b586451303d
ManojVirinchi <virinchimanoj@gmail.com>
2025-04-25 19:03:01 -0400

    first commit

---
+++ backend/src/main/java/com/taskmanager/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.taskmanager.backend.repository;
+
+import com.taskmanager.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
